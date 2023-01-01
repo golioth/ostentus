7f7ece8254c51d00ac118c2227067cb4af7e12b5
Mike Szczys <mike@golioth.io>
2022-12-31 21:12:54 -0500

    fix memory write pen color and register refresh
    * remove loop when only one register needs to be written to display
    * reset pen color before writing line to display
    * only refresh when data is actually received on reg addr 0x01
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- ostentus.py
+++ ostentus.py
@@ -37,9 +37,10 @@ def listen():
                     i2c.recv(refresh_byte, timeout=1000)
                 except OSError:
                     pass
-            if 0x00 <= refresh_byte[0] <= 0x03:
-                display.update_speed(refresh_byte[0])
-                display.update()
+
+                if 0x00 <= refresh_byte[0] <= 0x03:
+                    display.update_speed(refresh_byte[0])
+                    display.update()
 
         elif regAddress in [0x20, 0x21, 0x22, 0x23, 0x24, 0x25]:
             # Handle the master read/write request.
@@ -48,17 +49,19 @@ def listen():
                     data_addr = regAddress & 0xF
                     data[data_addr] = bytearray(b'\x00'*32)
                     i2c.recv(data[data_addr], timeout=1000)
+
                 except OSError:
                     pass
 
-                for i in range(len(data)):
-                    outstring=""
-                    for c in data[data_addr]:
-                        if c < 0x20 or c > 0x7E:
-                            outstring += ' '
-                        else:
-                            outstring += chr(c)
-                    display.text(outstring, 0, 10+(22*data_addr), 0.7)
+                display.pen(0)
+                outstring=""
+                for c in data[data_addr]:
+                    if c < 0x20 or c > 0x7E:
+                        outstring += ' '
+                    else:
+                        outstring += chr(c)
+                print("outstring:",outstring)
+                display.text(outstring, 0, 10+(22*data_addr), 0.7)
 
         else:
             continue

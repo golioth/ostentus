d623d322a11ff99efc55d8fccc99516f6225f914
Mike Szczys <mike@golioth.io>
2022-12-31 20:51:00 -0500

    add update speed to refresh command
    Writing a valid update speed value to register address 0x01 will refresh
    the ePaper display using the strings stored in memory. The value 0x00 is
    a full refresh with no ghosting, 0x03 is the fasted partical refresh
    which will exhibit ghosting. Values in between the two are a mix of
    speed versus fidelity.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Reference Designs.
 
 ## Installation
 
-Load [iversion 1.18.3 of the Badger2040 micropython
+Load [version 1.18.3 of the Badger2040 micropython
 build](https://github.com/pimoroni/pimoroni-pico/releases/tag/v1.18.3). The
 badge will enumerate as USB mass storage when the Boot button is held while
 reset is pressed and released. Copy the python file onto the drive and it will
@@ -25,14 +25,19 @@ Use any i2c-controller to write to the display on device-address 0x12.
 
 Memory register map:
 
+* 0x00: clear all memory (does not trigger a screen refresh)
 * 0x01: refresh display from memory
-* 0x02: clear all memory (does not trigger a screen refresh)
+  * Register value: 1-byte for [Badger update speed](https://github.com/pimoroni/pimoroni-pico/tree/main/micropython/modules/badger2040#update-speed-1)
+  * 0x00: UPDATE_NORMAL
+  * 0x01: UPDATE_MEDIUM
+  * 0x02: UPDATE_FAST
+  * 0x03: UPDATE_TURBO
 * 0x20: memory for line 0 on the display (memory max 32 characters)
 * ...
 * 0x26: memory for line 6 on the display (memory max 32 characters)
 
-The ont used is not monospace, so the 32 character max is a memory limit, not an
-idication of what will fit on the display.
+The font used is not monospace, so the 32 character max is a memory limit, not
+an indication of what will fit on the display.
 
 ## Example:
 
@@ -43,8 +48,12 @@ Using micropython on a Raspberry Pi Pico:
 [18]
 >>> i2c.writeto_mem(0x12, 0x20, "Hello World!")
 >>> i2c.writeto_mem(0x12, 0x22, "Golioth rulez!")
->>> i2c.writeto_mem(0x12, 0x01, "F")
+>>> i2c.writeto_mem(0x12, 0x01, b'\x00')
 ```
+
+This example scans for and prints out the ostentus i2c device (address 0x12)
+before writing to lines 0 and 2 in memory and using a full refresh to show those
+values on the display.
 
 ![Badger2040](img/ostentus_badger2040.jpg)
 
@@ -59,5 +68,5 @@ steps to build a more modern version:
 2. Run `make init` to initialize the library
 3. Enter the `micropython` library and run `git stash`
 4. Use `git checkout v1.18.3` to check out the correct version
-5. Go back to the i2cperipheral dirctory and run `make init`
+5. Go back to the i2cperipheral directory and run `make init`
 6. Run `make` to build i2cperipheral.mpy which can be placed on the Badger2040
--- ostentus.py
+++ ostentus.py
@@ -3,10 +3,11 @@ import badger2040
 
 def listen():
     display=badger2040.Badger2040()
-    display.font("serif")                                                                                                                                                     
+    display.font("serif")
     display.thickness(2)
 
     i2c = i2cperipheral.I2CPeripheral(bus=0, sclPin=5, sdaPin=4, address=0x12)
+    refresh_byte=bytearray(1)
     data = list()
     for i in range(7):
         data.append(bytearray(b'\x00'*32))
@@ -31,22 +32,16 @@ def listen():
             display.pen(15)
             display.clear()
         if regAddress == 0x01:
-            display.pen(15)
-            display.clear()
-            display.pen(0)
+            if i2c.have_recv_req():
+                try:
+                    i2c.recv(refresh_byte, timeout=1000)
+                except OSError:
+                    pass
+            if 0x00 <= refresh_byte[0] <= 0x03:
+                display.update_speed(refresh_byte[0])
+                display.update()
 
-            for i in range(len(data)):
-                outstring=""
-                for c in data[i]:
-                    if c < 0x20 or c > 0x7E:
-                        outstring += ' '
-                    else:
-                        outstring += chr(c)
-                display.text(outstring, 0, 10+(22*i), 0.7)
-            display.update()
-            #else:
-            #i2c.send(data, timeout=1000)
-        elif regAddress in [0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26]:
+        elif regAddress in [0x20, 0x21, 0x22, 0x23, 0x24, 0x25]:
             # Handle the master read/write request.
             if i2c.have_recv_req():
                 try:
@@ -55,8 +50,17 @@ def listen():
                     i2c.recv(data[data_addr], timeout=1000)
                 except OSError:
                     pass
+
+                for i in range(len(data)):
+                    outstring=""
+                    for c in data[data_addr]:
+                        if c < 0x20 or c > 0x7E:
+                            outstring += ' '
+                        else:
+                            outstring += chr(c)
+                    display.text(outstring, 0, 10+(22*data_addr), 0.7)
+
         else:
             continue
 
 listen()
-

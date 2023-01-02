9d9476919400a239b83caf93aed4b931d2abec68
Mike Szczys <mike@golioth.io>
2023-01-01 19:47:17 -0500

    enclose all functions in a class
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- ostentus.py
+++ ostentus.py
@@ -1,82 +1,104 @@
 import i2cperipheral
 import badger2040
 
-def listen():
-    display=badger2040.Badger2040()
-    display.font("serif")
-    display.thickness(2)
+class ostentus:
+    def __init__(self):
+        self.display=badger2040.Badger2040()
+        self.display.font("serif")
+        self.display.thickness(2)
 
-    i2c = i2cperipheral.I2CPeripheral(bus=0, sclPin=5, sdaPin=4, address=0x12)
-    clear_byte = bytearray(1)
-    refresh_byte = bytearray(1)
-    str_data = list()
-    for i in range(7):
-        str_data.append(bytearray(b'\x00'*32))
+        self.str_data = [bytearray(b'\x00'*32)]*6
+        x_loc = 0
+        y_loc = 0
 
-    print("Listening...")
-    while True:
-        regAddressBuff = bytearray(1)
+    def clear_memory(self):
+        self.str_data = [bytearray(b'\x00'*32)]*6
+        x_loc = 0
+        y_loc = 0
 
-        # First thing controller should send is register address.
-        # Poll to see if it has been received yet.
-        if not i2c.have_recv_req():
-            continue
-        i2c.recv(regAddressBuff, timeout=0)
+    def clear_str_memory(self, line_idx):
+        self.str_data[line_idx] = bytearray(b'\x00'*32)
 
-        # Wait for controller to send either the read or write.
-        while (not i2c.have_recv_req()) and (not i2c.have_send_req()):
-            pass
+    def write_string(self, line_idx):
+        self.display.pen(0)
+        outstring=""
+        for c in self.str_data[line_idx]:
+            if c < 0x20 or c > 0x7E:
+                outstring += ' '
+            else:
+                outstring += chr(c)
+        print("outstring:",outstring)
+        self.display.text(outstring, 0, 10+(22*line_idx), 0.7)
 
-        # Only support read/write requests for register 0x01.
-        regAddress = regAddressBuff[0]
-        if regAddress == 0x00:
-            if i2c.have_recv_req():
-                try:
-                    i2c.recv(clear_byte, timeout=1000)
+    def listen(self):
 
-                    display.pen(15)
-                    display.clear()
+        i2c = i2cperipheral.I2CPeripheral(bus=0, sclPin=5, sdaPin=4, address=0x12)
+        clear_byte = bytearray(1)
+        refresh_byte = bytearray(1)
+        coordinates = bytearray(2)
 
-                    if clear_byte[0] != 0x00:
-                        display.update_speed(0x00)
-                        display.update()
-                except OSError:
-                    pass
+        self.clear_memory()
+        print("Listening...")
+        while True:
+            regAddressBuff = bytearray(1)
 
-        if regAddress == 0x01:
-            if i2c.have_recv_req():
-                try:
-                    i2c.recv(refresh_byte, timeout=1000)
+            # First thing controller should send is register address.
+            # Poll to see if it has been received yet.
+            if not i2c.have_recv_req():
+                continue
+            i2c.recv(regAddressBuff, timeout=0)
 
-                    if 0x00 <= refresh_byte[0] <= 0x03:
-                        display.update_speed(refresh_byte[0])
-                        display.update()
+            # Wait for controller to send either the read or write.
+            while (not i2c.have_recv_req()) and (not i2c.have_send_req()):
+                pass
 
-                except OSError:
-                    pass
+            # Only support read/write requests for register 0x01.
+            regAddress = regAddressBuff[0]
+            if regAddress == 0x00:
+                if i2c.have_recv_req():
+                    try:
+                        i2c.recv(clear_byte, timeout=1000)
 
-        elif regAddress in [0x20, 0x21, 0x22, 0x23, 0x24, 0x25]:
-            # Handle the controller read/write request.
-            if i2c.have_recv_req():
-                try:
-                    data_addr = regAddress & 0xF
-                    str_data[data_addr] = bytearray(b'\x00'*32)
-                    i2c.recv(str_data[data_addr], timeout=1000)
+                        self.display.pen(15)
+                        self.display.clear()
 
-                except OSError:
-                    pass
+                        if clear_byte[0] != 0x00:
+                            self.display.update_speed(0x00)
+                            self.display.update()
+                    except OSError:
+                        pass
 
-                display.pen(0)
-                outstring=""
-                for c in str_data[data_addr]:
-                    if c < 0x20 or c > 0x7E:
-                        outstring += ' '
-                    else:
-                        outstring += chr(c)
-                print("outstring:",outstring)
-                display.text(outstring, 0, 10+(22*data_addr), 0.7)
+            if regAddress == 0x01:
+                if i2c.have_recv_req():
+                    try:
+                        i2c.recv(refresh_byte, timeout=1000)
 
-        else:
-            continue
+                        if 0x00 <= refresh_byte[0] <= 0x03:
+                            self.display.update_speed(refresh_byte[0])
+                            self.display.update()
 
-listen()
+                    except OSError:
+                        pass
+
+            elif regAddress in [0x20, 0x21, 0x22, 0x23, 0x24, 0x25]:
+                # Handle the controller read/write request.
+                if i2c.have_recv_req():
+                    try:
+                        data_addr = regAddress & 0xF
+                        self.str_data[data_addr] = bytearray(b'\x00'*32)
+                        i2c.recv(self.str_data[data_addr], timeout=1000)
+
+                    except OSError:
+                        pass
+
+                    self.write_string(data_addr)
+
+            else:
+                continue
+
+def main():
+    o = ostentus()
+    o.listen()
+
+if __name__ == "__main__":
+    main()

d146e92bcc4273aee5053e66154ede2920e08868
Mike Szczys <mike@golioth.io>
2023-02-15 16:19:04 -0600

    implemented partial update
    It is possible to update several areas at one time. For each subsequent
    partial update, all changed areas must be written again or prior partial
    update data will oscillate
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- drivers/ssd1681/ssd1681.cpp
+++ drivers/ssd1681/ssd1681.cpp
@@ -2,58 +2,15 @@
 
 #include <cstdlib>
 #include <math.h>
+#include <string.h>
 
 namespace pimoroni {
-
-  enum reg {
-    PSR      = 0x00,
-    PWR      = 0x01,
-    POF      = 0x02,
-    PFS      = 0x03,
-    PON      = 0x04,
-    PMES     = 0x05,
-    BTST     = 0x06,
-    DSLP     = 0x07,
-    DTM1     = 0x10,
-    DSP      = 0x11,
-    DRF      = 0x12,
-    DTM2     = 0x13,
-    LUT_VCOM = 0x20,
-    LUT_WW   = 0x21,
-    LUT_BW   = 0x22,
-    LUT_WB   = 0x23,
-    LUT_BB   = 0x24,
-    PLL      = 0x30,
-    TSC      = 0x40,
-    TSE      = 0x41,
-    TSR      = 0x43,
-    TSW      = 0x42,
-    CDI      = 0x50,
-    LPD      = 0x51,
-    TCON     = 0x60,
-    TRES     = 0x61,
-    REV      = 0x70,
-    FLG      = 0x71,
-    AMV      = 0x80,
-    VV       = 0x81,
-    VDCS     = 0x82,
-    PTL      = 0x90,
-    PTIN     = 0x91,
-    PTOU     = 0x92,
-    PGM      = 0xa0,
-    APG      = 0xa1,
-    ROTP     = 0xa2,
-    CCSET    = 0xe0,
-    PWS      = 0xe3,
-    TSSET    = 0xe5
-  };
 
   bool SSD1681::is_busy() {
     return gpio_get(BUSY);
   }
 
   void SSD1681::busy_wait() {
-    return;
     while(is_busy()) {
       tight_loop_contents();
     }
@@ -67,7 +24,7 @@ namespace pimoroni {
 
   void SSD1681::init() {
     // configure spi interface and pins
-    spi_init(spi, 12'000'000);
+    spi_init(spi, 20'000'000);
 
     gpio_set_function(DC, GPIO_FUNC_SIO);
     gpio_set_dir(DC, GPIO_OUT);
@@ -96,7 +53,6 @@ namespace pimoroni {
     busy_wait();
     command(0x12);  //SWRESET
     busy_wait();
-    sleep_ms(100);
 
     command(0x01, {0xC7, 0x00, 0x00}); //Driver output control
     command(0x11, {0x01}); //data entry mode
@@ -107,71 +63,6 @@ namespace pimoroni {
     command(0x4E, {0x00});   // set RAM x address count to 0;
     command(0x4F, {0xC7, 0x00});   // set RAM y address count to 0X199;
     busy_wait();
-
-//     _update_speed = speed;
-// 
-//     if(speed == 0) {
-//       command(PSR, {
-//         RES_128x296 | LUT_OTP | FORMAT_BW | SHIFT_RIGHT | BOOSTER_ON | RESET_NONE
-//       });
-//     } else {
-//       command(PSR, {
-//         RES_128x296 | LUT_REG | FORMAT_BW | SHIFT_RIGHT | BOOSTER_ON | RESET_NONE
-//       });
-//     }
-//     switch(speed) {
-//       case 0:
-// Note: the defult luts are built in so we don't really need to flash them here
-// they are preserved above for posterity and reference mostly.
-//         break;
-//       case 1:
-//         medium_luts();
-//         break;
-//       case 2:
-//         fast_luts();
-//         break;
-//       case 3:
-//         turbo_luts();
-//         break;
-//       default:
-//         break;
-//     }
-// 
-//     command(PWR, {
-//       VDS_INTERNAL | VDG_INTERNAL,
-//       VCOM_VD | VGHL_16V,
-//       0b101011,
-//       0b101011,
-//       0b101011
-//     });
-// 
-//     command(PON); // power on
-//     busy_wait();
-// 
-// booster soft start configuration
-//     command(BTST, {
-//       START_10MS | STRENGTH_3 | OFF_6_58US,
-//       START_10MS | STRENGTH_3 | OFF_6_58US,
-//       START_10MS | STRENGTH_3 | OFF_6_58US
-//     });
-// 
-//     command(PFS, {
-//       FRAMES_1
-//     });
-// 
-//     command(TSE, {
-//       TEMP_INTERNAL | OFFSET_0
-//     });
-// 
-//     command(TCON, {0x22}); // tcon setting
-//     command(CDI, {(uint8_t)(inverted ? 0b01'01'1100 : 0b01'00'1100)}); // vcom and data interval
-// 
-//     command(PLL, {
-//       HZ_100
-//     });
-// 
-//     command(POF);
-//     busy_wait();
   }
 
   void SSD1681::power_off() {
@@ -279,84 +170,73 @@ namespace pimoroni {
   }
 
   void SSD1681::partial_update(int x, int y, int w, int h, bool blocking) {
-    // y is given in columns ("banks"), which are groups of 8 horiontal pixels
-    // x is given in pixels
-//     if(blocking) {
-//       busy_wait();
-//     }
-// 
-//     int cols = h / 8;
-//     int y1 = y / 8;
-// int y2 = y1 + cols;
-// 
-//     int rows = w;
-//     int x1 = x;
-// int x2 = x + rows;
-// 
-//     uint8_t partial_window[7] = {
-//       (uint8_t)(y),
-//       (uint8_t)(y + h - 1),
-//       (uint8_t)(x >> 8),
-//       (uint8_t)(x & 0xff),
-//       (uint8_t)((x + w - 1) >> 8),
-//       (uint8_t)((x + w - 1) & 0xff),
-//       0b00000001  // PT_SCAN
-//     };
-//     command(PON); // turn on
-// 
-//     command(PTIN); // enable partial mode
-//     command(PTL, sizeof(partial_window), partial_window);
-// 
-//     command(DTM2);
-//     for (auto dx = 0; dx < rows; dx++) {
-//       int sx = dx + x1;
-//       int sy = y1;
-//       data(cols, &frame_buffer[sy + (sx * (height / 8))]);
-//     }
-//     command(DSP); // data stop
-// 
-//     command(DRF); // start display refresh
-// 
-//     if(blocking) {
-//       busy_wait();
-// 
-//       command(POF); // turn off
-//     }
+    if(blocking) {
+      busy_wait();
+    }
+
+    int cols = h / 8;
+    int y1 = y / 8;
+
+    int rows = w;
+    //Mirror the X axis
+    int x1 = width - x - 1;
+
+    uint8_t partial_window[6] = {
+      (uint8_t)y1,
+      (uint8_t)(y1 + cols - 1),
+      (uint8_t)(x1 & 0xff),
+      (uint8_t)(x1 >> 8),
+      (uint8_t)((x1 + w - 1) >> 8),
+      (uint8_t)((x1 + w - 1) & 0xff)
+    };
+
+    //Lock the border to prevent flashing
+    command(0x3C, {0x80}); //BorderWavefrom,
+
+    // set RAM x address start/end, in page 35
+    // RAM x address start at 00h;
+    // RAM x address end at 0fh(15+1)*8->128 
+    command(0x44, 2, partial_window);
+
+    // set RAM y address start/end, in page 35
+    // RAM y address start at 0127h;
+    // RAM y address start at 0127h;
+    // RAM y address end at 00h;
+    // ????=0
+    command(0x45, 4, partial_window+2);
+
+    command(0x4E, {partial_window[0]}); // set RAM x address count to 0;
+    command(0x4F, 2, partial_window+2); // set RAM y address count to 0X127;
+
+    command(0x24);   //Write Black and White image to RAM
+    for (auto dx = 0; dx < rows; dx++) {
+      int sx = dx + x;
+      int sy = y1;
+      data(cols, &frame_buffer[sy + (sx * (height / 8))]);
+    }
+
+    command(0x22, {0xFF});
+    command(0x20);
+    busy_wait();
   }
 
   void SSD1681::update(bool blocking) {
     if(blocking) {
       busy_wait();
     }
+
+    setup();
+
     command(0x24, (width * height) / 8, frame_buffer);
+    command(0x26, (width * height) / 8, frame_buffer);
     command(0x22, {0xF7});
     command(0x20);
     busy_wait();
-    command(0x22, {0x83});
-    command(0x20);
-    busy_wait();
-
-//     command(PON); // turn on
-// 
-//     command(PTOU); // disable partial mode
-// 
-//     command(DTM2, (width * height) / 8, frame_buffer); // transmit framebuffer
-//     command(DSP); // data stop
-// 
-//     command(DRF); // start display refresh
-
-//     if(blocking) {
-//       busy_wait();
-
-//       command(POF); // turn off
-//     }
   }
 
   void SSD1681::off() {
     busy_wait();
-//     command(POF); // turn off
     command(0x10, {0x01}); //enter deep sleep
     sleep_ms(100);
   }
-
 }

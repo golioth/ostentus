18b7654f4901c825fb8e9524c4ac338bf669dfe6
Mike Szczys <mike@golioth.io>
2023-02-24 13:25:25 -0600

    run even if cap touch is missing
    This commit makes sure that an error on the capactive touch i2c bus will
    not prevent the slideshow from running.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- module_py/cap_touch.py
+++ module_py/cap_touch.py
@@ -6,6 +6,7 @@ cap1203_sda = 2
 cap1203_scl = 3
 cap1203_bus = 1
 cap1203_int_pin = 7
+i2c_addr = 0x28
 
 # Workaround for PCB errors
 # (These pins are jumpered to 2/3)
@@ -22,8 +23,18 @@ touch_callback_left = None
 touch_callback_right = None
 touch_callback_top = None
 
-def clear_int(t):
-    i2c.writeto_mem(0x28, 0x00, b'\x00')
+def i2c_get_state():
+    try:
+        return i2c.readfrom_mem(0x28, 0x03, 0x01)
+    except Exception as e:
+        print("Cap touch i2c Error:", e)
+        return 0x00
+
+def i2c_clear_int(t=None):
+    try:
+        i2c.writeto_mem(i2c_addr, 0x00, b'\x00')
+    except Exception as e:
+        print("Cap touch i2c Error:", e)
 
 def register_callback_left(method_to_run):
     global touch_callback_left
@@ -48,8 +59,8 @@ def is_valid_callback(method_to_run):
 
 def irq_handler(pin):
     global touch_callback_left, touch_callback_right, touch_callback_top
-    state = i2c.readfrom_mem(0x28, 0x03, 0x01)
-    debounce_tim.init(mode=machine.Timer.ONE_SHOT, period=DEBOUNCE_MS, callback=clear_int)
+    state = i2c_get_state()
+    debounce_tim.init(mode=machine.Timer.ONE_SHOT, period=DEBOUNCE_MS, callback=i2c_clear_int)
 
     # FIXME: these are blocking calls (from an irq)
     if state[0] == 0x01 and touch_callback_left:
@@ -64,9 +75,10 @@ def start():
     if not touch_callback_left and not touch_callback_top and not touch_callback_right:
         print("cap_touch: Error starting, please register a callback first")
         return
-    i2c.writeto_mem(0x28, 0x00, b'\x00')
+    i2c_clear_int()
     touch_int.irq(handler=irq_handler, trigger=Pin.IRQ_FALLING)
 
 def stop():
     touch_int.irq(handler=None, trigger=0)
-    i2c.writeto_mem(0x28, 0x00, b'\x00')
+    i2c_clear_int()
+

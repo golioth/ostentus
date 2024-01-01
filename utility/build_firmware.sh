f6649da2fca67daad1d1eb5972afcfcea58196bf
Mike Szczys <mike@golioth.io>
2023-12-31 20:58:41 -0600

    update build to use new badger2040 repo
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- utility/build_firmware.sh
+++ utility/build_firmware.sh
@@ -3,10 +3,13 @@
 # Helper script used to execute build commands inside the docker container.
 # $ docker exec -i ostentus bash < utility/build_firmware.sh
 
+make BOARD=RPI_PICO submodules
+
 cmake -S . \
 		-B build-$BOARD_TYPE \
 		-DPICO_BUILD_DOCS=0 \
-		-DUSER_C_MODULES=$BADGER_COMPILE_DIR/pimoroni-pico/micropython/modules/badger2040-micropython.cmake \
+		-DUSER_C_MODULES=$BADGER_COMPILE_DIR/badger2040/firmware/PIMORONI_BADGER2040/micropython.cmake \
+		-DMICROPY_BOARD_DIR=$BADGER_COMPILE_DIR/badger2040/firmware/PIMORONI_BADGER2040 \
 		-DMICROPY_BOARD=$BOARD_TYPE \
 		-DCMAKE_C_COMPILER_LAUNCHER=ccache \
 		-DCMAKE_CXX_COMPILER_LAUNCHER=ccache

c84a3d7a8b146ec35b50615e8f33e54e3a7e4add
Mike Szczys <mike@golioth.io>
2024-03-05 14:34:09 -0600

    add support for Ostentus Rev B
    The new board remaps the capacitive touch interrupt pin and some of the
    LEDS. This commit enable captouch, and five single-color LEDs to deliver
    the same behavior as previous versions.
    
    Two enhancements are not yet implemented:
    * Multi-color support for Golioth and User LEDs
    * Accelerometer support
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- module/ostentus_i2c/led_ctrl.h
+++ module/ostentus_i2c/led_ctrl.h
@@ -4,8 +4,14 @@
 #define LED_POWER 1<<15
 #define LED_BATTERY 1<<14
 #define LED_INTERNET 1<<13
-#define LED_GOLIOTH 1<<12
-#define LED_USER 1<<11
+#define LED_GOLIOTH_R 1<<11
+#define LED_GOLIOTH_G 1<<12
+#define LED_GOLIOTH_B 1<<9
+#define LED_GOLIOTH LED_GOLIOTH_R
+#define LED_USER_R 1<<7
+#define LED_USER_G 1<<8
+#define LED_USER_B 1<<6
+#define LED_USER LED_USER_G
 #define LED_ALL_MASK (LED_POWER | LED_BATTERY | LED_INTERNET | LED_GOLIOTH | LED_USER)
 
 void led_init(void);
